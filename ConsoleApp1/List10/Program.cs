be1b8691b82d58dd588ef6a4d7834f9d7318ba95
jacoloves <5511068t@gmail.com>
2019-06-30 09:57:31 +0900

    20190630pra

---
+++ ConsoleApp1/List10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List10
+{
+    class Person
+    {
+        public string name;
+        public uint age;
+        public string address;
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+
+            p.name = "arai";
+            p.age = 17;
+            p.address = "http://araisanndayo.com";
+
+            Console.WriteLine("名前:{0}", p.name);
+            Console.WriteLine("年齢:{0}", p.age);
+            Console.WriteLine("アドレス:{0}", p.address);
+
+        }
+    }
+}
